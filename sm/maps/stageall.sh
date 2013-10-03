4c4d969adbfb2d1545104917256ef879cf74ef80
Peter A. Gustafson <peter.gustafson@wmich.edu>
2013-10-03 00:14:30 -0400

    Adding a missing pbs file.

--- sm/maps/stageall.sh
+++ sm/maps/stageall.sh
@@ -4,4 +4,5 @@ J=`qsub stagesec.pbs`
 J=`qsub -W depend=afterok:${J} stagetac.pbs`
 J=`qsub -W depend=afterok:${J} stageifr.pbs`
 J=`qsub -W depend=afterok:${J} stageifh.pbs`
+J=`qsub -W depend=afterok:${J} pyramids.pbs`
 J=`qsub -W depend=afterok:${J} databases.pbs`
