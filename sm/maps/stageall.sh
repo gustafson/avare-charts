ea27708bc8750637da2a87293df3808364c1210b
Peter A. Gustafson <peter.gustafson@wmich.edu>
2014-01-20 15:04:49 -0500

    First pass at cycle based tile structure.

--- sm/maps/stageall.sh
+++ sm/maps/stageall.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-[[ -d tiles ]] && rm -fr tiles
+if [[ $# -lt 1 ]]; then 
+    echo Cycle number argument required; 
+    exit
+fi
+
+[[ -d tiles/$1 ]] && rm -fr tiles/$1
 
 ## Do TAC separately since it takes the longest by a substantial margin
-J0=`qsub stagetac.pbs`
-J0=`qsub -W depend=afterok:${J0} pyramids-tac.pbs`
+J0=`qsub stagetac.pbs -v CYCLE=$1`
+J0=`qsub -W depend=afterok:${J0} pyramids-tac.pbs -v CYCLE=$1`
 
 ## Do the rest
-J1=`qsub stagesec.pbs`
-J2=`qsub stageifr.pbs`
-J3=`qsub stageifh.pbs`
-J4=`qsub stageifal.pbs`
-J5=`qsub stageifah.pbs`
-J6=`qsub -W depend=afterok:${J1}:${J2}:${J3}:${J4}:${J5} pyramids.pbs`
-J7=`qsub -W depend=afterok:${J0}:${J6} databases.pbs`
+J1=`qsub stagesec.pbs -v CYCLE=$1`
+J2=`qsub stageifr.pbs -v CYCLE=$1`
+J3=`qsub stageifh.pbs -v CYCLE=$1`
+J4=`qsub stageifal.pbs -v CYCLE=$1`
+J5=`qsub stageifah.pbs -v CYCLE=$1`
+J6=`qsub -W depend=afterok:${J1}:${J2}:${J3}:${J4}:${J5} pyramids.pbs -v CYCLE=$1`
+J7=`qsub -W depend=afterok:${J0}:${J6} databases.pbs -v CYCLE=$1`
