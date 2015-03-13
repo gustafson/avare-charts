be3f93a4add0f2f770d494496846a739e2c5fad7
apps4av <apps4av@gmail.com>
2015-03-13 11:50:17 -0400

    now vfr plates done with all python

--- sm/maps/streets.py
+++ sm/maps/streets.py
@@ -46,6 +46,106 @@ def todb(values,proc):
 	    if con:
 		con.close() 
 
+def set_map(m,fname):
+
+    # White BG
+    m.background = Color('white')
+
+    # A style that shows
+    s = Style()
+
+    # rules
+
+    # rule for runway
+    r1 = Rule()
+    f1 = Filter("[aeroway] = 'runway'")
+    r1.filter = f1
+
+    ln1 = LineSymbolizer()
+    stk1 = Stroke(Color('black'),1.0)
+    stk1.width = 8;
+    ln1.stroke = stk1
+    
+    tx1 = TextSymbolizer(Expression('[ref]'), 'DejaVu Sans Book', 20, Color('red'))
+    tx1.halo_fill = Color('white')
+    tx1.halo_radius = 2
+    tx1.label_placement = label_placement.LINE_PLACEMENT
+    dir(tx1)
+    
+    tx12 = TextSymbolizer(Expression('[name]'), 'DejaVu Sans Book', 20, Color('red'))
+    tx12.halo_fill = Color('white')
+    tx12.halo_radius = 2
+    tx12.label_placement = label_placement.LINE_PLACEMENT
+    dir(tx12)
+    
+    r1.symbols.append(ln1)
+    r1.symbols.append(tx1)
+    r1.symbols.append(tx12)
+    s.rules.append(r1)
+
+    # rule for taxiway
+    r2 = Rule()
+    f2 = Filter("[aeroway] = 'taxiway'")
+    r2.filter = f2
+
+    ln2 = LineSymbolizer()
+    stk2 = Stroke(Color('green'),1.0)
+    stk2.width = 2;
+    stk2.line_join = line_join.ROUND_JOIN
+    stk2.line_cap = line_cap.ROUND_CAP
+    ln2.stroke = stk2
+    
+    tx2 = TextSymbolizer(Expression('[ref]'), 'DejaVu Sans Book', 16, Color('red'))
+    tx2.halo_fill = Color('white')
+    tx2.halo_radius = 2
+    tx2.label_placement = label_placement.LINE_PLACEMENT
+    dir(tx2)
+    
+    tx22 = TextSymbolizer(Expression('[name]'), 'DejaVu Sans Book', 16, Color('red'))
+    tx22.halo_fill = Color('white')
+    tx22.halo_radius = 2
+    tx22.label_placement = label_placement.LINE_PLACEMENT
+    dir(tx22)
+    
+    r2.symbols.append(ln2)
+    r2.symbols.append(tx2)
+    r2.symbols.append(tx22)
+    s.rules.append(r2)
+   
+    # rule for others
+    r3 = Rule()
+    f3 = Filter("(not [aeroway] = 'runway') and (not [aeroway] = 'taxiway')")
+    r3.filter = f3
+
+    ln3 = LineSymbolizer()
+    stk3 = Stroke(Color('blue'),1.0)
+    stk3.width = 1;
+    stk3.line_join = line_join.ROUND_JOIN
+    stk3.line_cap = line_cap.ROUND_CAP
+    ln3.stroke = stk3
+    
+    tx3 = TextSymbolizer(Expression('[name]'), 'DejaVu Sans Book', 14, Color('black'))
+    tx3.halo_fill = Color('white')
+    tx3.halo_radius = 1
+    tx3.label_placement = label_placement.LINE_PLACEMENT
+    dir(tx3)
+    
+    r3.symbols.append(ln3)
+    r3.symbols.append(tx3)
+    s.rules.append(r3)
+
+
+    # Put style in
+    m.append_style('avare style',s)
+
+    # single layer
+    layer = Layer('avare layer')
+
+    # file to load data from, put in layer
+    ds = Osm(file=fname)
+    layer.datasource = ds
+    layer.styles.append('avare style')
+    m.layers.append(layer)
 
 def do_one(airport,lonc,latc):
 
@@ -78,11 +178,8 @@ def do_one(airport,lonc,latc):
 	    url = "http://overpass-api.de/api/map?bbox=" + str(lonl) + "," + str(latd) + "," + str(lonr) + "," + str(latu)
 	    urllib.urlretrieve (url, airport + ".osm")
         
-	# Prepare to create
-	shutil.copyfile(airport + ".osm", "map.osm")
-
 	m = Map(x,y)
-	load_map(m, mapfile)
+	set_map(m, airport + ".osm")
 
 	# projection
 	m.srs = outproj.params()
@@ -114,6 +211,7 @@ def do_one(airport,lonc,latc):
 	# Show done
 	print airport + " done" 
 
-# Main
-do_one(str(sys.argv[1]),float(sys.argv[2]),float(sys.argv[3]))
+# Main looking for something like BED|-71.289|42.4699444444444 direct from database
+inp = sys.argv[1].split("|")
+do_one(str(inp[0]),float(inp[1]),float(inp[2]))
 
