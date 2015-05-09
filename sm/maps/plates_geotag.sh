d8ab2ac42231182e23df0baaba1c962024a15640
Peter A. Gustafson <peter.gustafson@wmich.edu>
2015-05-08 23:19:09 -0400

    Updating pbs script for more parallelism.

--- sm/maps/plates_geotag.sh
+++ sm/maps/plates_geotag.sh
@@ -4,28 +4,40 @@
 
 ## Note to extract to svg without inkscape, pstoedit seems to be the best at preserving text rather than glyph.  But it looks pretty ugly and it gets both the glyph (as polygon outline) and the text.  Grep to get rid of the polygon.
 
-##
-
-if [[ $# -ne 1 ]] ; then NP=32; else NP=1; fi
+if [[ $# -eq 0 ]]; then
+    NP=32
+else
+    NP=$1
+fi
 
 function svgtext(){
 cat << EOF
 <svg height="1238" width="806" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" onload="Init(evt)">
 <defs>
 <style type="text/css"><![CDATA[ 
+	    line.approaches {
+		stroke: #00f;
+		stroke-width: 5;
+		stroke-opacity:1;
+	    }
+	    line.runways {
+		stroke: #f00;
+		stroke-width: 1.6;
+		stroke-opacity:.4;
+	    }
 	    ellipse.airport {
 		stroke: #0ff;
-		stroke-width: 1;
-		stroke-opacity:.5;
+		stroke-width: .1;
+		stroke-opacity:.1;
 		fill: #0ff;
-		fill-opacity:.0;
+		fill-opacity:.1;
 	    }
 	    circle.airport {
 		stroke: #f00;
-		stroke-width: 1.5;
-		stroke-opacity:.4;
+		stroke-width: .1;
+		stroke-opacity:.1;
 		fill: #f00;
-		fill-opacity:.2;
+		fill-opacity:.1;
 	    }
 	    circle.navaid {
 		stroke: #0f0;
@@ -44,37 +56,37 @@ cat << EOF
 	    circle.obs {
 		stroke: #0ff;
 		stroke-width: 1.2;
-		stroke-opacity:.15;
+		stroke-opacity:.10;
 		fill: #0ff;
-		fill-opacity:.15;
+		fill-opacity:.1;
 	    }
 	    text.airport {
 		stroke: #f00;
 		fill: #f00;
 		text-anchor:"middle";
                 font-size:75%;
-                opacity:0.25;
+                opacity:0.1;
 	    }
 	    text.navaid {
 		stroke: #0f0;
 		fill: #0f0;
 		text-anchor:"middle";
                 font-size:75%;
-                opacity:0.25;
+                opacity:0.1;
 	    }
 	    text.fix {
 		stroke: #00f;
 		fill: #00f;
 		text-anchor:"middle";
                 font-size:75%;
-                opacity:0.25;
+                opacity:0.1;
 	    }
 	    text.obs {
 		stroke: #0ff;
 		fill: #0ff;
 		text-anchor:"middle";
                 font-size:60%;
-                opacity:0.25;
+                opacity:0.1;
 	    }
 	    ]]></style>
 </defs>
@@ -121,7 +133,11 @@ function doit(){
 
     IFS=\| read proc state <<< "$0"
 
-    png=plates.archive/$(./cyclenumber.sh)/plates_${state}/${proc};
+    if [[ `echo $proc | grep -o AREA.png` ]]; then
+	png=plates.archive/area/${proc};
+    else
+	png=plates.archive/$(./cyclenumber.sh)/plates_${state}/${proc};
+    fi
     pdf=`echo $png |sed s/.png/.pdf/`
     pngf=$(echo $png |cut -f2-3 -d"/")
     pngf=plates/$pngf
@@ -133,17 +149,19 @@ function doit(){
     ## echo png $png $pngf  
     ## echo svg $svg
 
-    if [[ -f $pdf ]]; then
+    if [[ -f $png ]]; then
 
 	unset SVGTEXT
 	
 	## Gather the array of constants
 	## 806x1238
-	
+	read mywidth myheight <<< $(convert $png -ping -format '%[fx:w] %[fx:h]' info:)
+
  	minmax=($(\
- 	sqlite3 geoplates.db "select proc,lon,dx,lat,dy from geoplates where proc=='$proc';" | head -n1 |
- 		awk 'BEGIN {FS="|" } ;{printf ("%g %g %g %g %g %g", $2, $2+806.0/$3, $4, $4+1238.0/$5, $3, $5)}'
- 		))
+ 		       sqlite3 geoplates.db "select proc,lon,dx,lat,dy from geoplates where proc=='$proc';" | head -n1 |
+ 			   awk -v mywidth=$mywidth -v myheight=$myheight 'BEGIN {FS="|" } ;{printf ("%g %g %g %g %g %g", $2, $2+mywidth/$3, $4, $4+myheight/$5, $3, $5)}'
+ 		   ))
+	## echo ${minmax[0]} ${minmax[1]} ${minmax[2]} ${minmax[3]} ${minmax[4]} ${minmax[5]}
 
 	## Check if the domain is reasonable for a plate
 	TOOLARGE=`echo ${minmax[0]} ${minmax[1]} ${minmax[2]} ${minmax[3]} | 
@@ -156,7 +174,9 @@ function doit(){
 	    echo WARNING: $proc is bad
 	fi
 	## echo ${minmax[*]} 
-	
+
+
+
  	AIRPORT=`echo $proc |cut -f1 -d/`;
 	APPTYPE=`echo $proc |cut -c 5-7`;
 	RUNWAY=`echo $proc |cut -c 8-|grep -o '[0-9][0-9]'`
@@ -180,24 +200,40 @@ function doit(){
 
 	COL="255,0,0";
 	
-	SVGTEXT=$(svgtext)
-	
-	    ## Embedd a file or a link to a file
-	    ## SVGTEXT+="<image xlink:href=\"$pngf\" x=\"0\" y=\"0\" height=\"1238px\" width=\"806px\"/>"
-	    ## SVGTEXT+="<image xlink:href=\"$pdff\" x=\"0\" y=\"0\" height=\"1238px\" width=\"806px\"/>"
+	if [[ `echo $proc | grep -o AREA.png` ]]; then
+	    APPL=2000
+	    APPW=174.31
+	    SVGTEXT=$(svgtext|sed -e s/806/$mywidth/ -e s/1238/$myheight/ -e s/"stroke-width: 1.6"/"stroke-width: 20.0"/)
+	else
+	    APPL=100
+	    APPW=8.7156
+	    APPL=200
+	    APPW=17.431
+	    APPL=1000
+	    APPW=87.156
+	    SVGTEXT=$(svgtext|sed -e s/806/$mywidth/ -e s/1238/$myheight/ -e s/"stroke-width: 1.6"/"stroke-width: 2.0"/)
+	fi
+
+
+	## echo STARTING THE AIRPORT SECTION
+	## Embedd a file or a link to a file
+	## SVGTEXT+="<image xlink:href=\"$pngf\" x=\"0\" y=\"0\" height=\"1238px\" width=\"${mywidth}px\"/>"
+	## SVGTEXT+="<image xlink:href=\"$pdff\" x=\"0\" y=\"0\" height=\"1238px\" width=\"${mywidth}px\"/>"
 	
 	SVGTEXT+="<image xlink:href=\"data:image/png;base64,"
 	SVGTEXT+=`convert $png png:fd:1|base64`
-	SVGTEXT+="\" x=\"0\" y=\"0\" height=\"1238px\" width=\"806px\"/>"
+	SVGTEXT+="\" x=\"0\" y=\"0\" height=\"${myheight}px\" width=\"${mywidth}px\"/>"
 	
 	SVGTEXT+="<g id=\"all\">"
-	SVGTEXT+="<rect width=\"806\" height=\"1238\" style=\"fill:rgba(0,0,0,0); stroke-width:3; stroke:rgba(0,0,0,0)\" onclick='ToggleOpacity(evt, \"all\")'/>"
+	SVGTEXT+="<rect width=\"${mywidth}\" height=\"${myheight}\" style=\"fill:rgba(0,0,0,0); stroke-width:3; stroke:rgba(0,0,0,0)\" onclick='ToggleOpacity(evt, \"all\")'/>"
+
 	SVGTEXT+="<g id=\"airports\" onclick='ToggleOpacity(evt, \"airports\")'>"
 	SVGTEXT+="<circle class=\"airport\" cx=\"${X}\" cy=\"${Y}\" r=\"7.5\"/>"
 	[[ `echo $pngf |grep NDB` ]] && SVGTEXT+="<ellipse class=\"airport\" cx=\"${X}\" cy=\"${Y}\" rx=\"${RX}\" ry=\"${RY}\"/>"
 	SVGTEXT+="<text class=\"airport\" x=\"${X}\" y=\"${Y}\">${CIR[0]}</text>"
 	SVGTEXT+="</g>"
-	
+
+
 	X=$(printf "%.0f\n" $X)
 	Y=$(printf "%.0f\n" $Y)
 	
@@ -205,18 +241,19 @@ function doit(){
 	CMD+=" -draw \"translate ${X},${Y} circle 0,0 10,0\""
 	CMD+=" -fill \"rgba(${COL},1)\" -pointsize 15 -annotate +${X}+${Y} ${CIR[0]}"
 	
-	PDFTEXT=`pdftotext $pdf -`;
-	if [[ `echo "$PDFTEXT"|wc -l` -gt 5 ]]; then USEPDF=1; else USEPDF=0; fi
 
+	PDFTEXT=`pdftotext -q $pdf -`;
+	if [[ `echo "$PDFTEXT"|wc -l` -gt 5 ]]; then USEPDF=1; else USEPDF=0; fi
+	
 	FORCE=0;
 	if [[ $TOOLARGE -eq 0 ]]; then
 	    if [[ $USEPDF -eq 1 ]]; then 
 		nav=`sqlite3 main.db "select LocationID from nav where ARPLongitude>${minmax[0]} and ARPLongitude<${minmax[1]} and ARPLatitude<${minmax[2]} and ARPLatitude>${minmax[3]};"`
-		nav2=`pdftotext $pdf -| grep -o -F "$nav" |sort |uniq`
+		nav2=`pdftotext -q $pdf -| grep -o -F "$nav" |sort |uniq`
 	    else
 		nav=`sqlite3 procedures.db "select InitialCourse, FinalCourse, MissedCourse from procedures where Airport like '%$AIRPORT' and AppType like '%$APPTYPE%' and Runway like '%$RUNWAY%';"`
 		nav2=`echo $nav |sed -e "s/,/\n/g" -e "s/|/\n/g" -e "s/ //g" | sed -e "/^$/d" | sort | uniq`
-
+		
 		## What if there are not enough?
 		if [[ `echo "$nav2" |wc -w` -lt 2 ]]; then
  		    nav=`sqlite3 main.db "select LocationID from nav where ARPLongitude>${minmax[0]} and ARPLongitude<${minmax[1]} and ARPLatitude<${minmax[2]} and ARPLatitude>${minmax[3]};"`
@@ -227,6 +264,48 @@ function doit(){
 	fi
 
 
+	## echo DRAWING THE RUNWAYS
+	RUNWAYENDS=`sqlite3 main.db "select HELongitude,HELatitude,LELongitude,LELatitude from airportrunways where LocationID=='$AIRPORT'"`
+ 	SVGTEXT+="<g id=\"runways\" onclick='ToggleOpacity(evt, \"runways\")'>"
+	while IFS=\| read X1 Y1 X2 Y2; do
+	    X1=`echo ${X1} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+	    X2=`echo ${X2} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+ 	    Y1=`echo ${Y1} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+ 	    Y2=`echo ${Y2} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+	    SVGTEXT+="<line class=\"runways\" x1=\"${X1}\" y1=\"${Y1}\" x2=\"${X2}\" y2=\"${Y2}\"/>"
+	done <<< "$RUNWAYENDS"
+	SVGTEXT+="</g>"
+
+	## ## echo DRAW AN APPROACH PATH with dashes
+ 	## SVGTEXT+="<g fill=\"none\" stroke=\"blue\" stroke-width=\"10\" onclick='ToggleOpacity(evt, \"approaches\")'>"
+	## while IFS=\| read X1 Y1 X2 Y2; do
+	##     X1=`echo ${X1} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+	##     X2=`echo ${X2} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+ 	##     Y1=`echo ${Y1} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+ 	##     Y2=`echo ${Y2} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+	##     X3=`echo ${X1} ${X2} |awk '{print 10*($1-$2)+$1}'`
+	##     Y3=`echo ${Y1} ${Y2} |awk '{print 10*($1-$2)+$1}'`
+	##     SVGTEXT+="<path stroke-dasharray=\"10,10\" d=\"M${X1} ${Y1} ${X3} ${Y3}\" />"
+	## done <<< "$RUNWAYENDS"
+	## SVGTEXT+="</g>"
+
+	## echo DRAW AN APPROACH PATH triangle
+ 	SVGTEXT+="<g style=\"fill:#f00; fill-opacity:0.05; stroke: #f00; stroke-opacity: 0.2; stroke-width:0.1\" onclick='ToggleOpacity(evt, \"approaches\")'>"
+	while IFS=\| read X1 Y1 X2 Y2; do
+	    X1=`echo ${X1} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+	    X2=`echo ${X2} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+ 	    Y1=`echo ${Y1} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+ 	    Y2=`echo ${Y2} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+	    THETA1=`echo ${X1} ${X2} ${Y1} ${Y2} |awk '{print atan2(($3-$4),($1-$2))*(180/atan2(0, -1))}'`
+	    THETA2=`echo ${X1} ${X2} ${Y1} ${Y2} |awk '{print 180+atan2(($3-$4),($1-$2))*(180/atan2(0, -1))}'`
+	    SVGTEXT+="<polygon points=\"0,0 ${APPL},-${APPW} ${APPL},${APPW}\" transform=\"translate(${X1},${Y1}) rotate(${THETA1},0,0)\"/>"
+	    SVGTEXT+="<polygon points=\"0,0 ${APPL},-${APPW} ${APPL},${APPW}\" transform=\"translate(${X2},${Y2}) rotate(${THETA2},0,0)\"/>"
+	done <<< "$RUNWAYENDS"
+	SVGTEXT+="</g>"
+
+
+
+	## echo STARTING THE NAVAIDS SECTION
 	    ##[[ "plates/GYY/ILS-OR-LOC-RWY-30.png" == $pngf ]]     && nav2=`printf "CGT"` && FORCE=1;
 
 	    ## GREPSTR="\"`echo $nav| sed s/" "/"\\\\\|"/g;`\""
@@ -244,7 +323,7 @@ function doit(){
 	    DXT=`echo $DX |awk '{print $1 + 7.5}'`
 	    DYT=`echo $DY |awk '{print $1 - 7.5}'`
 	    
-	    if [[ `pdftotext $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
+	    if [[ `pdftotext -q $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
 	 	SVGTEXT+="<circle class=\"navaid\" cx=\"${DX}\" cy=\"${DY}\" r=\"7.5\"/>"
 	 	SVGTEXT+="<text class=\"navaid\" x=\"${DXT}\" y=\"${DYT}\">${CIR[0]}</text>"
 	    fi
@@ -252,7 +331,7 @@ function doit(){
 	    DX=$(printf "%.0f\n" $DX)
 	    DY=$(printf "%.0f\n" $DY)
 	    
-	    if [[ `pdftotext $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
+	    if [[ `pdftotext -q $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
 	 	CMD+=" -stroke \"rgba(${COL},0.60)\" -fill \"rgba(${COL},0.40)\" -strokewidth 2 -draw 'translate ${DX},${DY} circle 0,0 10,0'";
 	 	CMD+=" -fill \"rgba(${COL},1)\" -annotate +${DX}+${DY} ${CIR[0]}";
 	    fi
@@ -264,7 +343,7 @@ function doit(){
 	if [[ $TOOLARGE -eq 0 ]]; then 
 	    if [[ $USEPDF -eq 1 ]]; then 
 		fix=`sqlite3 main.db "select LocationID from fix where ARPLongitude>${minmax[0]} and ARPLongitude<${minmax[1]} and ARPLatitude<${minmax[2]} and ARPLatitude>${minmax[3]};"`
-		fix2=`pdftotext $pdf -| grep -o -F "$fix" |sort |uniq`
+		fix2=`pdftotext -q $pdf -| grep -o -F "$fix" |sort |uniq`
 	    else
 		fix=`sqlite3 procedures.db "select InitialCourse, FinalCourse, MissedCourse from procedures where Airport like '%$AIRPORT' and AppType like '%$APPTYPE%' and Runway like '%$RUNWAY%';"`
 		fix2=`echo $fix |sed -e "s/,/\n/g" -e "s/|/\n/g" -e "s/ //g" | sed -e "/^$/d" | sort | uniq`
@@ -293,7 +372,6 @@ function doit(){
 
 	## Fixes
 	SVGTEXT+="<g id=\"fixes\" onclick='ToggleOpacity(evt, \"fixes\")'>"
-	    #`pdftotext $pdf - |grep -o $GREPSTR |uniq`
 	for fix in $fix2; do
 	    if [[ $TOOLARGE -eq 0 ]]; then 
 		## echo $fix $pdff
@@ -307,7 +385,7 @@ function doit(){
 		DXT=`echo $DX |awk '{print $1 + 7.5}'`
 		DYT=`echo $DY |awk '{print $1 - 7.5}'`
 	 	
-	 	if [[ `pdftotext $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
+	 	if [[ `pdftotext -q $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
 	 	    SVGTEXT+="<circle class=\"fix\" cx=\"${DX}\" cy=\"${DY}\" r=\"7.5\"/>"
 	 	    SVGTEXT+="<text class=\"fix\" x=\"${DXT}\" y=\"${DYT}\">${CIR[0]}</text>"
 	 	fi
@@ -316,8 +394,8 @@ function doit(){
 	  	DY=$(printf "%.0f\n" $DY)
 	  	DXT=$(printf "%.0f\n" $DXT)
 	  	DYT=$(printf "%.0f\n" $DYT)
-  	
-	 	if [[ `pdftotext $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
+  		
+	 	if [[ `pdftotext -q $pdf - |grep -c ${CIR[0]}` > 0 || $FORCE -eq 1 ]]; then
 	 	    CMD+=" -stroke \"rgba(${COL},0.60)\" -fill \"rgba(${COL},0.40)\" -strokewidth 2 -draw 'translate ${DX},${DY} circle 0,0 10,0'";
 	 	    CMD+=" -fill \"rgba(${COL},1)\" -annotate +${DXT}+${DYT} ${CIR[0]}";
 	 	fi
@@ -329,31 +407,31 @@ function doit(){
 
 
 
-	obs=`sqlite3 main.db "select ARPLongitude,ARPLatitude,Height from obs where ARPLongitude>${minmax[0]} and ARPLongitude<${minmax[1]} and ARPLatitude<${minmax[2]} and ARPLatitude>${minmax[3]};"`
-	obs2=`echo "$obs" |sort |uniq`
-	SVGTEXT+="<g id=\"obs\" onclick='ToggleOpacity(evt, \"obs\")'>"
-	
-	for obs in $obs2; do
-	    IFS=\| read lon lat height <<< "$obs"    
-
-	    lon=$(printf "%.4f\n" $lon)
-	    lat=$(printf "%.4f\n" $lat)
-	    height=$(printf "%.0f\n" $height)
-
-	    COL="0,255,255"
-	    
-	    DX=`echo ${lon} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
-	    DY=`echo ${lat} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
-	    DXT=`echo $DX |awk '{print $1 + 5.5}'`
-	    DYT=`echo $DY |awk '{print $1 - 5.5}'`
-
-	    SVGTEXT+="<circle class=\"obs\" cx=\"${DX}\" cy=\"${DY}\" r=\"7.5\"/>"
-	    SVGTEXT+="<text class=\"obs\" x=\"${DXT}\" y=\"${DYT}\">${height}</text>"
-	    
-	    CMD+=" -stroke \"rgba(${COL},0.60)\" -fill \"rgba(${COL},0.40)\" -strokewidth 1 -draw 'translate ${DX},${DY} circle 0,0 5,0'";
-	    ## CMD+=" -fill \"rgba(${COL},1)\" -annotate +${DX}+${DY} ${height}";
-	done
-	SVGTEXT+="</g>"
+ 	obs=`sqlite3 main.db "select Height,ARPLongitude,ARPLatitude from obs where ARPLongitude>${minmax[0]} and ARPLongitude<${minmax[1]} and ARPLatitude<${minmax[2]} and ARPLatitude>${minmax[3]} order by Height desc limit 100;"`
+ 	#obs2=`echo "$obs" |sort`
+ 	SVGTEXT+="<g id=\"obs\" onclick='ToggleOpacity(evt, \"obs\")'>"
+ 	
+ 	for obs in $obs; do
+ 	    IFS=\| read height lon lat <<< "$obs"    
+ 
+ 	    lon=$(printf "%.4f\n" $lon)
+ 	    lat=$(printf "%.4f\n" $lat)
+ 	    height=$(printf "%.0f\n" $height)
+ 
+ 	    COL="0,255,255"
+ 	    
+ 	    DX=`echo ${lon} ${minmax[0]} ${minmax[4]}|awk '{print ($1-$2)*$3}'`
+ 	    DY=`echo ${lat} ${minmax[2]} ${minmax[5]}|awk '{print ($1-$2)*$3}'`
+ 	    DXT=`echo $DX |awk '{print $1 + 5.5}'`
+ 	    DYT=`echo $DY |awk '{print $1 - 5.5}'`
+ 
+ 	    SVGTEXT+="<circle class=\"obs\" cx=\"${DX}\" cy=\"${DY}\" r=\"7.5\"/>"
+ 	    SVGTEXT+="<text class=\"obs\" x=\"${DXT}\" y=\"${DYT}\">${height}</text>"
+ 	    
+ 	    CMD+=" -stroke \"rgba(${COL},0.60)\" -fill \"rgba(${COL},0.40)\" -strokewidth 1 -draw 'translate ${DX},${DY} circle 0,0 5,0'";
+ 	    ## CMD+=" -fill \"rgba(${COL},1)\" -annotate +${DX}+${DY} ${height}";
+ 	done
+ 	SVGTEXT+="</g>"
 
 
 	SVGTEXT+="</g>"
@@ -366,7 +444,7 @@ function doit(){
 	CMD1+=" $png ${png}.geotags.png"
 	
 	    ## CMD+=" -antialias -depth 8 -quality 00 -colors 31 -size 806x1238 xc:transparent -alpha Background ${proc}.alpha.png"
-	CMD+=" -size 806x1238 xc:transparent -colors 256 png8:${png}.alpha.png"
+	CMD+=" -size ${mywidth}x${myheight} xc:transparent -colors 256 png8:${png}.alpha.png"
 	
 	    ## For PNG files
 	    ## eval $CMD
@@ -376,7 +454,7 @@ function doit(){
 	echo "$SVGTEXT" > $svg
 
     else
-	echo missing $pdf for $proc: removing from databases
+	echo missing $png for $proc: this proceedures should be removed from the database
 	## sqlite3 -init init.sql validation.db "begin; delete from geoplatesvalidation where proc='$proc'; commit;";
 	## sqlite3 -init init.sql geoplates.db "begin; delete from geoplates where proc='$proc'; commit;";
     fi
@@ -508,10 +586,12 @@ export -f assembleit
 ## VECTOR SVGS FROM PDF ## fi
 
 
-echo Creating geotags
-sqlite3 geoplates.db "select proc,state from geoplates;" > /dev/shm/tmp.txt
-cat /dev/shm/tmp.txt | 
+echo Creating geotags from existing plates
+sqlite3 geoplates.db "select proc,state from geoplates;" | sed -n "$NP~16p" |
 xargs -P${NP} -n1 bash -c doit
 wait
 
-rm /dev/shm/tmp.txt
+## echo Creating geotags from area plates \(openstreetmap\)
+## sqlite3 geoplates.db "select proc,state from geoplates where proc like '%AREA%';" | 
+## xargs -P${NP} -n1 bash -c doit
+## wait
