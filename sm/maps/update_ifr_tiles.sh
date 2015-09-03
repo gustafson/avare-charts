e8479f829c06b5b0b71c91935b2f5f1635c94766
Peter A. Gustafson <peter.gustafson@wmich.edu>
2015-09-03 14:54:36 -0400

    Removing tools from old projection which are obsolete.  Updating to run on a smaller machine.

--- sm/maps/update_ifr_tiles.sh
+++
@@ -1,9 +0,0 @@
-CURRENT=`./cyclenumber.sh`
-let PAST=$CURRENT-1;
-echo $CURRENT $PAST
-
-[[ -d tiles/$CURRENT ]] || mkdir tiles/$CURRENT
-
-for a in 3 4 5; do 
-    [[ -d tiles/$PAST/$a ]] && mv tiles/$PAST/$a tiles/$CURRENT/$a
-done
