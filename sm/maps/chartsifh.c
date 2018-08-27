10370e41a8ae69cec0eee546ef478ed7ef883f9a
Peter A. Gustafson <peter.gustafson@wmich.edu>
2018-08-27 15:47:27 -0400

    IFR charts mods to complete the prior commit.

--- sm/maps/chartsifh.c
+++ sm/maps/chartsifh.c
@@ -1,10 +1,10 @@
 Maps maps[] = {
   // The first charts go underneath, the later charts go on top in any stack
-  {"ENR_P01", 2204, 257,  2950, 4526, "IFAL"},
-  {"ENR_P01", 5155, 257,  12641, 7482, "IFAL"},
-  {"ENR_AKH01",  2204, 264, 19587, 4014, "IFAH"}, // Same chart split into two pieces for efficiency
-  {"ENR_AKH01",  2204, 264, 10655, 7471, "IFAH"}, // Same chart split into two pieces for efficiency
-  {"ENR_AKH02",  2206, 266, 19590, 7470, "IFAH"},
+  {"ENR_P01", 2204, 257,  2950, 4526, "IFAL"}, // This includes GUAM and is cut in to pieces to exclude the GUAM area chart attached to it.  The first wraps over the dateline
+  {"ENR_P01", 5155, 257,  12641, 7482, "IFAL"}, // 
+  {"ENR_AKH01",  2204, 264, 19587, 4014, "IFAH"}, // Same chart split into two pieces to exclude the seattle inset
+  {"ENR_AKH01",  2204, 264, 10655, 7471, "IFAH"}, 
+  {"ENR_AKH02",  2206, 266, 19590, 7470, "IFAH"}, // This wraps over the date line
   {"ENR_H01",  2205, 205, 21691, 7591, "IFH"},
   {"ENR_H02",  2105, 205, 19791, 7591, "IFH"},
   {"ENR_H03",  2205, 100, 21691, 7696, "IFH"},
