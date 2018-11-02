4cba464fb362e315cb9f06d2f43d6a727a9bf5af
Peter A. Gustafson <peter.gustafson@wmich.edu>
2018-11-02 10:40:39 -0400

    Adding a pbs file for plates to be managed by torque.  Updates to plate code.

--- sm/maps/plates/unzip_DDTPP.sh
+++ sm/maps/plates/unzip_DDTPP.sh
@@ -4,7 +4,7 @@ pushd $1
 for zip in ../*zip; do
     echo ${zip}
     RETURN=0;
-    [[ `unzip -qqo ${zip} *PDF; echo $?` -eq 0 ]] || RETURN=1
+    [[ `unzip -qqo ${zip} "*PDF"; echo $?` -eq 0 ]] || RETURN=1
 done
 
 unzip ../DDTPPE*.zip d-TPP_Metafile.xml || RETURN 1
