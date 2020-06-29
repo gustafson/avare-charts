ddf4b75d1fef8ef2c3630cd9d1d9094ee2164170
Peter A. Gustafson <peter.gustafson@wmich.edu>
2020-06-29 12:17:45 -0400

    Update required to process alternate mins with 2008 DDTPP.

--- charts/plates/unzip_DDTPP.sh
+++ charts/plates/unzip_DDTPP.sh
@@ -1,12 +1,19 @@
-[[ -d $1 ]] || mkdir $1
-pushd $1
+[[ -d ${1} ]] || mkdir ${1}
+pushd ${1}
 
 for zip in ../*zip; do
     echo ${zip}
     RETURN=0;
-    [[ `unzip -qqo ${zip} "*PDF"; echo $?` -eq 0 ]] || RETURN=1
+    ## Deal with differing file extensions that have occasionally occurred
+    for TYPE in pdf PDF; do
+	if [[ `unzip -qql ${zip} "*${TYPE}"` ]]; then
+	    [[ `unzip -qqo ${zip} "*${TYPE}"; echo $?` -eq 0 ]] || RETURN=1
+	fi
+    done
 done
 
-unzip ../DDTPPE*.zip d-TPP_Metafile.xml || RETURN=1
+[[ -f ${1}/*pdf ]] && rename pdf PDF ${1}/*pdf
+
+unzip -qq ../DDTPPE*.zip d-TPP_Metafile.xml || RETURN=1
 
 echo $RETURN
